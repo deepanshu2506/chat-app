28d54f96c4e1770d76e237e4c3634826841eb136
Deepanshu Vangani <vanganideepanshu@gmail.com>
2019-12-08 17:25:56 +0530

    added mysql depenency

--- webpages/scripts/register_validation.js
+++ webpages/scripts/register_validation.js
@@ -1,9 +1,9 @@
 const nameRegex = /^[a-zA-Z]+$/;
 const usernameRegex = /^[a-zA-Z0-9._]+$/;
-var error = {'fnameError':false , 'lnameError':false , 'unameError':false , 'passwordError':false , 'repasswordError' :false};
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var error = {'fnameError':false , 'lnameError':false , 'unameError':false,'emailError':false,'mobileError':false , 'passwordError':false , 'repasswordError' :false};
 
 $('#firstname').on('keyup',function(event){
-    console.log($(this).val() != '')
     if($(this).val() != '' && !nameRegex.test($(this).val()) && !error.fnameError){
         
             $(this).after('<span>*enter only alphabets</span>');
@@ -17,7 +17,7 @@ $('#firstname').on('keyup',function(event){
 });
 
 $('#lastname').on('keyup',function(event){
-    console.log($(this).val() != '')
+
     if($(this).val() != '' && !nameRegex.test($(this).val()) && !error.lnameError){
         
             $(this).after('<span>*enter only alphabets</span>');
@@ -31,9 +31,9 @@ $('#lastname').on('keyup',function(event){
 });
 
 $('#username').on('keyup',function(event){
-    console.log($(this).val() != '')
-    if($(this).val() != '' && !usernameRegex.test($(this).val()) && !error.lnameError){
-            $(this).after('<span>*only a-z ,0-9, . , _ are allowed </span>');
+
+    if($(this).val() != '' && !usernameRegex.test($(this).val()) && !error.unamenameError){
+        $(this).after('<span>*only a-z ,0-9, . , _ are allowed </span>');
         error.unameError = true;
     }else{
         if($(this).val() == '' || usernameRegex.test($(this).val())){
@@ -43,3 +43,40 @@ $('#username').on('keyup',function(event){
     }
 });
 
+$('#email').on('focusout',function(event){
+
+    if($(this).val() != '' && !emailRegex.test($(this).val()) && !error.emailError){
+            $(this).after('<span>*Enter valid email id </span>');
+        error.emailError = true;
+    }else{
+        if($(this).val() == '' || emailRegex.test($(this).val())){
+            $(this).next('span').remove();
+            error.emailError = false;
+        }
+    }
+});
+
+//add password validations
+// $('#password').on('keyup',function(event){
+//     console.log($(this).val() != '')
+//     if($(this).val() != '' && !emailRegex.test($(this).val()) && !error.emailError){
+//             $(this).after('<span>*Enter valid email id </span>');
+//         error.unameError = true;
+//     }else{
+//         if($(this).val() == '' || emailRegex.test($(this).val())){
+//             $(this).next('span').remove();
+//             error.unameError = false;
+//         }
+//     }
+// });
+
+$('#repassword').on('focusout',function(event){
+    if($(this).val() != $('#password').val()){
+            console.log($(this).val() +" "+ $('#password').val());
+            $(this).after('<span>*passwords do not match</span>');
+        error.unameError = true;
+    }else{
+            $(this).next('span').remove();
+            error.unameError = false;
+    }
+});
